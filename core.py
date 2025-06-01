43d1e0965f1b60e7e913a824daeda4e6ce72da7b
Dhanush A <164708105+dhanuvagman006@users.noreply.github.com>
2025-06-01 07:59:09 +0530

    added core files

---
+++ core.py
@@ -0,0 +1,33 @@
+from openai import OpenAI
+
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key="<OPENROUTER_API_KEY>",
+)
+
+completion = client.chat.completions.create(
+  extra_headers={
+    "HTTP-Referer": "https://www.nexusclubs.in/", # Optional. Site URL for rankings on openrouter.ai.
+    "X-Title": "<NEXUS_SIT", 
+  },
+  extra_body={},
+  model="meta-llama/llama-4-maverick:free",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What is in this image?"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+          }
+        }
+      ]
+    }
+  ]
+)
+print(completion.choices[0].message.content)
